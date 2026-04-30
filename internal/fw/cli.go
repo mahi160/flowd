ec2a8fc677373d25acb564cf11fc54699c71b3e8
mahi160 <omarsifat288@gmail.com>
2026-04-30 17:59:14 +0600

    feat(cli): add --version flag

--- internal/fw/cli.go
+++ internal/fw/cli.go
@@ -1,10 +1,18 @@
 package fw
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+// Version is set at build time via:
+//
+//	go build -ldflags "-X github.com/mahi160/flowd/internal/fw.Version=1.2.3" ./cmd/fw
+//
+// Falls back to "dev" when building without the flag.
+var Version = "dev"
 
 var (
 	cfgPath string
@@ -21,6 +29,8 @@ func Run() {
 	}
 	root.PersistentFlags().StringVar(&cfgPath, "config", DefaultConfigPath(), "config file path")
 	root.PersistentFlags().BoolVar(&debug, "debug", false, "enable debug logging")
+	root.Version = Version
+	root.SetVersionTemplate(fmt.Sprintf("fw %s\n", Version))
 	root.AddCommand(
 		cmdInit(), cmdStart(), cmdStop(), cmdStatus(),
 		cmdSummary(), cmdReport(), cmdDashboard(), cmdSetupTmux(),
