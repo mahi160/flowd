bb07cd312a1f788b61fe80d5052f9a0af8dab1d9
mahi160 <omarsifat288@gmail.com>
2026-05-14 08:31:56 +0600

    feat: new dashboard init

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,17 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [svelte(), viteSingleFile(), tailwindcss()],
+  plugins: [svelte(), tailwindcss(), viteSingleFile()],
   build: {
     emptyOutDir: true,
     outDir: "../internal/fw/static",
-    minify: 'terser',
-  },
-  server: {
-    port: 5173,
-    strictPort: false,
   },
 });
